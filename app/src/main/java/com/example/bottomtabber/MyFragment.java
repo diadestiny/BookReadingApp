051eeed813879071b714918d498e5ea500569fda
diadestiny <1247889154@qq.com>
2019-07-14 17:03:22 +0800

    first

---
+++ app/src/main/java/com/example/bottomtabber/MyFragment.java
@@ -0,0 +1,29 @@
+package com.example.bottomtabber;
+
+import android.annotation.SuppressLint;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+@SuppressLint("ValidFragment")
+public class MyFragment extends Fragment {
+    private String content;
+    @SuppressLint("ValidFragment")
+    public MyFragment(String content){
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content,container,false);
+        TextView txt_content = view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
